d0f1e637cc240dcae0da8b0c08169b3c109a37c3
rohithjack <78618053+rohithjack@users.noreply.github.com>
2021-05-02 14:59:49 +0530

    Add files via upload

---
+++ EmployeeWageCAlculation.java
@@ -0,0 +1,65 @@
+package com.Day3;
+
+
+
+import java.lang.Math;
+
+class EmployeeWageCAlculation {
+
+    public static void main(String args[])
+    {
+  // define the range
+        int max = 1;
+        int min = 0;
+        int range = max - min + 1;
+
+        // generate random number
+
+           int rand = (int)(Math.random() * range) + min;  //UC1 -CHECK EMPLOYEE PRESENT OR ABSANT
+                     int rand1 = (int)(Math.random() * range) + min;
+            switch (rand){     // UC4--SOLVING USING SWITCH STATEMENT
+                case 0:
+                    System.out.println("Employee Absant " );
+
+
+                    break;
+                case 1:
+                    if(rand1!=0) {
+                        System.out.println ( "Daily Wage  Employee " );  //UC2 -DAILY WAGE EMPLOYEE
+                        int wagePerHour=20;
+                        int noofWorkingHoursPerDay=8;
+                        int workingDaysinMonth=20;
+
+                        int empSalaryperDay = noofWorkingHoursPerDay*wagePerHour;
+                        System.out.println ( "Employee salaray per day is $" + empSalaryperDay );
+                        int monthlySalary = empSalaryperDay* workingDaysinMonth;  //UC5 AND UC6 --CALCULATING WAGES FOR
+                        // MONTH
+                        System.out.println ( "Employee salaray per motnth is $" +monthlySalary  );
+
+                    }
+                    else {
+                        System.out.println ( "Part time Employee " );   ////UC3 -PART TIME EMPLOYEE
+                        int  wagePerHour=8;
+                        int noofWorkingHoursPerDay=8;
+                        int workingDaysinMonth=20;
+
+                        int empSalaryperDay = noofWorkingHoursPerDay*wagePerHour;
+                        System.out.println ( "Employee salaray per day is $" + empSalaryperDay );
+                        int monthlySalary = empSalaryperDay* workingDaysinMonth;
+                        System.out.println ( "Employee salaray per motnth is $" +monthlySalary  );
+                    }
+
+                    break;
+
+
+                default: //code to be executed if all cases are not matched
+
+                    System.out.println("Invalid " );
+
+            }
+
+
+
+
+    }
+}
