35812776f0807fdf8911e1faa6b1a803edd02f56
rohithjack <78618053+rohithjack@users.noreply.github.com>
2021-05-03 00:37:13 +0530

    Update Day3LineComparision.java

--- Day3LineComparision.java
+++ Day3LineComparision.java
@@ -5,59 +5,53 @@ import java.util.Scanner;
 import java.io.*;
 import java.lang.Math;
 
-class LineComparision {
-    float x,y,x1, x2, y1, y2 ,x3,y3;
-    float line;
-    float diff;
-    double num;
+ public class Main {
+
+     public static void main(String[] args) {
 
 
-    public void disp() {
-        System.out.println ( "Line comaprision problem " );
-    }
-
-    public void line() {
-        Scanner input = new Scanner ( System.in );
-        System.out.println ( "Enter line x1,y1 " );
-        float x1 = input.nextFloat ();
-        float y1 = input.nextFloat ();
-        System.out.println ( "Enter line x2,y2 " );
-        float x2 = input.nextFloat ();
-        float y2 = input.nextFloat ();
-        x=(x2-x1);
-        y=(y2-y1 );
-
-        x3=x*x;
-        y3=y*y;
+         float x, y, x1, x2, y1, y2, x3, y3;
+         float line;
+         float diff;
+         double num;
 
 
-        line = x3+y3;
 
-        System.out.println ( " x1is  "  +x1);
-        System.out.println ( " x2 is  "  +x2);
-        System.out.println ( " y1 is  "  +y1 );
-        System.out.println ( " y2 is  "  +y2 );
-
-        System.out.println ( " x2-x1 is  "  +x );
-        System.out.println ( " y2-y1 is  "  +y );
-        System.out.println ( "(x2-x1)^2 + (y2-y1)^2 is "  +line );
-        System.out.println ( " Length of a line " +Math.sqrt(line));
+         System.out.println ( "Line comaprision problem " );
 
 
-    }
 
-}
+             Scanner input = new Scanner ( System.in );
+             System.out.println ( "Enter line x1,y1 " );
+              x1 = input.nextFloat ();
+             y1 = input.nextFloat ();
+             System.out.println ( "Enter line x2,y2 " );
+             x2 = input.nextFloat ();
+             y2 = input.nextFloat ();
+             x = (x2 - x1);
+             y = (y2 - y1);
 
- class Main {
-
-    public static void main(String[] args) {
-
-        LineComparision obj = new LineComparision();
-        obj.disp();
-        obj.line();
+             x3 = x * x;
+             y3 = y * y;
 
 
-    }
-}
+             line = x3 + y3;
+
+             System.out.println ( " x1is  " + x1 );
+             System.out.println ( " x2 is  " + x2 );
+             System.out.println ( " y1 is  " + y1 );
+             System.out.println ( " y2 is  " + y2 );
+
+             System.out.println ( " x2-x1 is  " + x );
+             System.out.println ( " y2-y1 is  " + y );
+             System.out.println ( "(x2-x1)^2 + (y2-y1)^2 is " + line );
+             System.out.println ( " Length of a line " + Math.sqrt ( line ) );
+
+
+
+
+     }
+ }
+
 
 
